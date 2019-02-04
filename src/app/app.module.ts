d37bb6cbe88496e51fdcef0654d2a4cb9483092e
mukantwarivirginie <virgm2020@gmail.com>
2019-02-04 15:42:49 +0200

    created generate class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientFormComponent } from './client-form/client-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
